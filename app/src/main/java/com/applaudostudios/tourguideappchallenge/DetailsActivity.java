24dabb058ef190bdbd8b8811f4ad89615942199d
Nelson Quintanilla <nelson.quinta@hotmail.com>
2018-09-13 10:42:40 -0600

    up button done

--- app/src/main/java/com/applaudostudios/tourguideappchallenge/DetailsActivity.java
+++ app/src/main/java/com/applaudostudios/tourguideappchallenge/DetailsActivity.java
@@ -1,7 +1,11 @@
 package com.applaudostudios.tourguideappchallenge;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
