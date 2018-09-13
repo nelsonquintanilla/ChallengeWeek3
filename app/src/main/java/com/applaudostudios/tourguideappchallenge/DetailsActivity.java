3135c4046c0a21d5b587134a32e93fef4db5e509
Nelson Quintanilla <nelson.quinta@hotmail.com>
2018-09-13 17:54:42 -0600

    phone listener

--- app/src/main/java/com/applaudostudios/tourguideappchallenge/DetailsActivity.java
+++ app/src/main/java/com/applaudostudios/tourguideappchallenge/DetailsActivity.java
@@ -1,10 +1,14 @@
 package com.applaudostudios.tourguideappchallenge;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -16,6 +20,17 @@ public class DetailsActivity extends AppCompatActivity {
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        ImageView phone = findViewById(R.id.phone_button);
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                String temp = "tel:" + "+503-2277-7777";
+                intent.setData(Uri.parse(temp));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/applaudostudios/tourguideappchallenge/HikingFragment.java
+++ app/src/main/java/com/applaudostudios/tourguideappchallenge/HikingFragment.java
@@ -1,6 +1,5 @@
 package com.applaudostudios.tourguideappchallenge;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
