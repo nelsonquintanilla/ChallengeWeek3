7d92c6c5a51c2a5466ab5c93a206734f4a0ce7dc
Nelson Quintanilla <nelson.quinta@hotmail.com>
2018-09-13 17:34:16 -0600

    details xml

--- app/src/main/java/com/applaudostudios/tourguideappchallenge/LakesAndWaterFallsFragment.java
+++ app/src/main/java/com/applaudostudios/tourguideappchallenge/LakesAndWaterFallsFragment.java
@@ -3,6 +3,7 @@ package com.applaudostudios.tourguideappchallenge;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -28,7 +29,7 @@ public class LakesAndWaterFallsFragment extends Fragment implements CallBack {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_lakes_and_waterfalls, container, false);
